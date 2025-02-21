b0af05e9afcb7354b6ccb1956ed60b4ef63da40c
samiksha603 <samikshamohapatra7@gmail.com>
2025-02-21 20:09:29 +0530

    Added

---
+++ src/Events.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Events = () => {
+  return (
+    <div className="events-page">
+      <h1>Events</h1>
+      <ul>
+        <li>Python Challenge</li>
+        <li>Web Development</li>
+        <li>Data Science</li>
+        <li>Algorithm Contest</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Events;
---
+++ src/Rules.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rules = () => {
+  return (
+    <div className="rules-page">
+      <h1>Rules</h1>
+      <ol>
+        <li>No cheating allowed.</li>
+        <li>Submit your code before the deadline.</li>
+        <li>Respect other participants.</li>
+      </ol>
+    </div>
+  );
+};
+
+export default Rules;
